44f48e9cf3a8c8735cddf2bf6ef683cb79af09e7
재준 전 <wjswowns1234@outlook.com>
2020-12-19 03:49:41 +0900

    asd

---
+++ src/schema.js
@@ -0,0 +1,15 @@
+import { makeExecutableSchema } from "graphql-tools";
+import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
+import { loadFilesSync } from "@graphql-tools/load-files";
+
+import path from "path";
+
+const allTypes = loadFilesSync(path.join(__dirname, "/api/**/*.graphql")); //api 있는 모든 폴더/파일 .graphql 찾기
+const allResolvers = loadFilesSync(path.join(__dirname, "/api/**/*.js")); //api 있는 모든 폴더/파일 .js 찾기
+
+const schema = makeExecutableSchema({
+    typeDefs: mergeTypeDefs(allTypes),
+    resolvers: mergeResolvers(allResolvers),
+});
+
+export default schema;
---
+++ src/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config(); //.env 외부환경변수사용 process.env.~ 으로 접근
+import { GraphQLServer } from "graphql-yoga"; //graphql server
+import logger from "morgan"; // 미들웨어
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev")); //요청로그
+
+server.start({ port: PORT }, () => console.log(`✅ Server Running on http://localhost:${PORT}`));
