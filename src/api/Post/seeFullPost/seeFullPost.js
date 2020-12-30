c70f5f93049c3cefb766df964658f7d2c7d764f2
재준 전 <wjswowns1234@outlook.com>
2020-12-31 01:27:47 +0900

    fragment refactoring

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,28 +1,11 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT } from "../../../fragment";
+import { COMMENT_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragment";
 
 export default {
     Query: {
         seeFullPost: async (_, args) => {
             const { id } = args;
-            const post = await prisma.post({ id: id });
-            const comments = await prisma
-                .post({ id: id })
-                .comments()
-                .$fragment(COMMENT_FRAGMENT);
-            const likeCount = await prisma
-                .likesConnection({ where: { post: { id: id } } })
-                .aggregate()
-                .count();
-            const files = await prisma.post({ id: id }).files();
-            const user = await prisma.post({ id: id }).user();
-            return {
-                post,
-                comments,
-                likeCount,
-                user,
-                files,
-            };
+            return await prisma.post({ id: id }).$fragment(FULL_POST_FRAGMENT);
         },
     },
 };
