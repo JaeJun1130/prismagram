db1b4fa5eb7c1f7a5d3138f485f9633f6ab27f88
재준 전 <wjswowns1234@gmail.com>
2021-01-07 20:21:23 +0900

    create Account

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,10 +2,24 @@ import { prisma } from "../../../../generated/prisma-client";
 //회원가입
 export default {
     Mutation: {
-        createAccount: async (_, args_) => {
-            const { username, email, fistName = "", lastName = "", bio = "" } = args;
-            const user = await prisma.createUser({ username: username, email, fistName, lastName, bio });
-            return user;
+        createAccount: async (_, args) => {
+            const { username, email, firstName = "", lastName = "", bio = "" } = args;
+            const check = await prisma.$exists.user({
+                OR: [{ username: username }, { email: email }],
+            });
+            if (check == true) {
+                console.log("이미 존재하는 계정입니다.");
+                return false;
+            } else {
+                await prisma.createUser({
+                    username: username,
+                    email,
+                    firstName,
+                    lastName,
+                    bio,
+                });
+                return true;
+            }
         },
     },
 };
